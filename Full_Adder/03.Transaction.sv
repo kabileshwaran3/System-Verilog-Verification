c49fd70a222a7205adcd3d5ecfe7b911bfee8e9e
kabileshwaran <skabilesh231@gmail.com>
2025-10-29 19:15:49 +0530

    Add transaction class for full adder simulation

---
+++ Full_Adder/03.Transaction.sv
@@ -0,0 +1,14 @@
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit cin;
+  
+  bit sum;
+  bit carry;
+  
+  
+  function void display (string name);
+    $display("%s",name);
+    $display($time,"  a=%b,b=%b, cin=%b, sum=%b, carry=%b",a,b,cin,sum,carry);
+  endfunction
+endclass
